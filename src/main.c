1aadec42be2ca0172fff45bde3804411296b8ccd
GreGorn <grzegorz.gornisiewicz@gmail.com>
2025-11-19 13:29:25 +0100

    Command not found message

--- src/main.c
+++ src/main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Captures the user's command in the "command" variable
+char command[1024];
+
 int main(int argc, char *argv[])
 {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
+  // prompt
   printf("$ ");
+
+  // get command
+  fgets(command, sizeof(command), stdin);
+
+  // Remove the trailing newline
+  command[strcspn(command, "\n")] = '\0';
+
+  // Prints the "<command>: command not found" message
+  printf("%s: command not found\n", command);
 
   return 0;
 }
