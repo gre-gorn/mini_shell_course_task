545134de3283456d47e9b14e1d9036d768902af5
GreGorn <grzegorz.gornisiewicz@gmail.com>
2025-11-19 13:46:41 +0100

    Infinite loop with prompting after users input

--- src/main.c
+++ src/main.c
@@ -9,18 +9,21 @@ int main(int argc, char *argv[])
 {
   // Flush after every printf
   setbuf(stdout, NULL);
+  int exit = 1;
+  do
+  {
+    // prompt
+    printf("$ ");
 
-  // prompt
-  printf("$ ");
+    // get command
+    fgets(command, sizeof(command), stdin);
 
-  // get command
-  fgets(command, sizeof(command), stdin);
+    // Remove the trailing newline
+    command[strcspn(command, "\n")] = '\0';
 
-  // Remove the trailing newline
-  command[strcspn(command, "\n")] = '\0';
-
-  // Prints the "<command>: command not found" message
-  printf("%s: command not found\n", command);
+    // Prints the "<command>: command not found" message
+    printf("%s: command not found\n", command);
+  } while (exit);
 
   return 0;
 }
